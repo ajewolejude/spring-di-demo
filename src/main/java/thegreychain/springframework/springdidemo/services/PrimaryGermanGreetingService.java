c6fb27a1abf70028c9da13a3b8df4d919c87071f
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2019-05-21 11:34:29 +0100

    Dependency Injection - Using Default Profile

--- src/main/java/thegreychain/springframework/springdidemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/thegreychain/springframework/springdidemo/services/PrimaryGermanGreetingService.java
@@ -6,12 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-
-public class PrimaryGreetingService implements GreetingService {
-
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello-  This was injected by a primary bean";
+        return "Primärer Grußdienst";
     }
 }
